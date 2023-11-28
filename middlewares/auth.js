da9c56d67f33f79d6997eb562e058ed40118c39e
pnyekrasov <pnekrasov1910@gmail.com>
2023-11-28 15:53:58 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ const { JWT_SECRET } = process.env;
 const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ", 2);
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
   try {
